bc3e1f2aabb94e4551bdb4912a783f046473627e
Matheus Nogueira Albuquerque <127986753+Nogz04@users.noreply.github.com>
2024-12-11 19:29:45 -0300

    Add files via upload

---
+++ ProjetoGerenciaEmprestimo/GerenciamentoEmprestimoItensUFN/src/conexao/Conexao.java
@@ -0,0 +1,32 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.*;
+
+
+
+public class Conexao {
+    
+    
+    public Connection getConexao(){ 
+        
+        try{
+            
+            
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/GerenciamentoEmprestimo2?Timezone=true&serverTimezone=UTC", "root", "matheuszinho123");
+            
+            System.out.println("Conexão realizada com sucesso!");
+            return conn;
+
+            
+            
+        }catch(Exception e){
+            System.out.println("Erro ao conectar-se ao Banco de Dados" +e.getMessage());
+            return null;
+        }
+        
+        
+        
+    }
+    
+}
